16382fc2e049ee76b1d12ddfcb5dfba055dcbb04
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-01-29 22:57:51 +0100

    feat: add demo for indexed rendering

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,9 @@
+#include <string>
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+using namespace std;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
@@ -25,7 +28,116 @@ void processInput(GLFWwindow* window) {
 	}
 }
 
+void run_program_2(GLFWwindow* window) {
+
+	float vertices[] = {
+	 0.5f, 0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 -0.5f,  -0.5f, 0.0f,
+	 -0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 3, 
+		1, 2, 3
+	};
+
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+}
+
+void run_program_1(GLFWwindow* window) {
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
+	};
+
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+string select_program() {
+	cout << "Please specify the program to run:\n\n";
+	cout << "(1) Lesson 1-1: Triangle drawing (glDrawArrays)\n";
+	cout << "(2) Lesson 1-2: Triangle drawing indexed (glDrawElements)\n";
+	cout << "(q to quit)\n\n";
+
+	string option;
+
+	cin >> option;
+
+	if (option != "q") {
+		cout << "running " + option + "...\n";
+	}
+	
+	return option;
+}
+
 int main() {
+
+	string option = select_program();
+
+	if (option == "q") {
+		return 0;
+	}
 
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -50,13 +162,6 @@ int main() {
 	}
 
 	glViewport(0, 0, 800, 600);
-
-
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f
-	};
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -103,33 +208,15 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	while (!glfwWindowShouldClose(window)) {
-
-		processInput(window);
-
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+	if (option == "1") {
+		run_program_1(window);
+	}
+	else {
+		run_program_2(window);
 	}
 
 	glfwTerminate();
-	return 0;
+
+	main();
+
 }
